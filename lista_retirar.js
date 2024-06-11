36139633778bd75aa31aaba301bbd2c4ebff4be4
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-06-11 09:57:45 -0300

    Add files via upload

---
+++ lista_retirar.js
@@ -0,0 +1,7 @@
+var numero = ['01', '02', '03', '04', '05'];
+
+ //Removendo o último elemento do array usando o método pop
+ var ultimonumero = numero.pop();
+
+ console.log('Array após remover o último elemento:', numero); //Saída: ['01, '02', '03, '04'
+ console.log('Último numero removido:', ultimonumero); //Saída: '05'
